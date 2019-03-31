86deb38aaa4dfff172361b7510ff398ded5d7653
Rebecca Sun <sunruoyun96@163.com>
2019-03-31 15:12:02 +0000

    mongodb

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var User=require("./models/user")
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-    
-mongoose.connect("mongodb://localhost:27017/yelp_camp_app", { useNewUrlParser: true }); 
+ 
+var url=process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_app"   
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true }); 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"));
